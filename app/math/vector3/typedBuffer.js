0376e6cb39dae45955f1d8ddef19b8fd95982cbd
Maciej Makowski <cfiet@cfiet.net>
2013-01-14 22:24:36 +0100

    Working typed vector interface

--- app/math/vector3/typedBuffer.js
+++ app/math/vector3/typedBuffer.js
@@ -4,28 +4,96 @@
   var BufferFactory = Float32Array,
     Vector3;
 
-  function getterSetter (offset) {
-    var vector = this;
-    return function (value) {
-      if (!value) {
-        return vector.data[offset];
-      }
-      return vector.data[offset] = value;
-    };
-  }
-
   Vector3 = function () {
-    function Vector3 (initialArray) {
+    var fromArray = function (arr) {
+      return fromNumber(arr[0], arr[1], arr[2]);
+    };
+
+    var fromObject = function (o) {
+      return fromNumber(o.x, o.y, o.z);
+    };
+
+    function Vector3 (/* typedArray || array || {x, y, z} || x, y, z */) {
       this.data = new BufferFactory(3);
-      if (initialArray) {
-        this.data.set(initialArray, 0);
+      var param;
+      if (arguments.length === 1) {
+        param = arguments[0];
+        if (param.buffer instanceof ArrayBuffer || param instanceof Array) {
+          this.data.set(param);
+        } else if (param instanceof Object) {
+          this.x = param.x;
+          this.y = param.y;
+          this.z = param.z;
+        }
+      }
+      else if (arguments.length === 3) {
+        this.x = arguments[0];
+        this.y = arguments[1];
+        this.z = arguments[2];
       }
     }
 
     Vector3.prototype = {
-      x: getterSetter(0),
-      y: getterSetter(1),
-      z: getterSetter(2)
+      get x () {
+        return this.data[0];
+      },
+      set x (value) {
+        this.data[0] = value;
+      },
+      get y () {
+        return this.data[1];
+      },
+      set y (value) {
+        this.data[1] = value;
+      },
+      get z () {
+        return this.data[2];
+      },
+      set z (value) {
+        this.data[2] = value;
+      },
+      get length () {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+      },
+      add: function (right) {
+        return new Vector3(
+          this.x + right.x,
+          this.y + right.y,
+          this.z + right.z
+        );
+      },
+      mul: function (scalar) {
+        return new Vector3(
+          this.x * scalar,
+          this.y * scalar,
+          this.z * scalar
+        );
+      },
+      unit: function () {
+        var len = this.length;
+        return new Vector3(
+          this.x / len,
+          this.y / len,
+          this.z / len
+        );
+      },
+      inverted: function () {
+        return new Vector3(
+          -this.x,
+          -this.y,
+          -this.z
+        );
+      },
+      sub: function (rigth) {
+        return new Vector3(
+          this.x - rigth.x,
+          this.y - rigth.y,
+          this.z - rigth.z
+        );
+      },
+      div: function (scalar) {
+        return this.mul(1 / scalar);
+      }
     };
 
     return Vector3;
--- test/math/vector3/typedBuffer.js
+++ test/math/vector3/typedBuffer.js
@@ -2,43 +2,222 @@
 (function () {
   "use strict";
 
-  var Vector3 = require("math/vector3/typedBuffer");
+  var Vector3 = require("math/vector3/typedBuffer"),
+    DELTA = 0.001;
 
   describe("Vector3 on typed buffers", function () {
 
-    it("is a function (object facotry)", function () {
+    it("is a function (object factory)", function () {
       expect(Vector3).to.be.a("function");
     });
 
-    describe("default creation", function () {
-      var vector = new Vector3();
+    describe("construction", function () {
+      describe("default creation", function () {
+        var vector = new Vector3();
 
-      it("creates data buffer", function () {
-        expect(vector.data).to.be.instanceOf(Float32Array);
+        it("creates data buffer", function () {
+          expect(vector.data).to.be.instanceOf(Float32Array);
+        });
+
+        it("initializes the buffer as (0,0,0)", function () {
+          _([0, 1, 2]).each(function (i) {
+            expect(vector.data[i]).to.be.equal(0);
+          });
+        });
       });
 
-      it("initializes the buffer as (0,0,0)", function () {
-        _([0, 1, 2]).each(function (i) {
-          expect(vector.data[i]).to.be.equal(0);
+      describe("typed array based creation", function () {
+        var arr, vector;
+        arr = new Float32Array(3);
+        arr.set([Math.E, Math.PI, Math.SQRT2]);
+
+        vector = new Vector3(arr);
+
+        it("creates new buffer view", function () {
+          expect(vector.data).to.be.instanceOf(Float32Array);
+          expect(vector.data).not.to.be.equal(arr);
+        });
+
+        it("initializes buffer with values from original buffer", function () {
+          _([0, 1, 2]).each(function (i) {
+            expect(vector.data[i]).to.be.equal(arr[i]);
+          });
+        });
+      });
+
+      describe("array based creation", function () {
+        var arr, vector;
+        arr = [Math.E, Math.PI, Math.SQRT2];
+        vector = new Vector3(arr);
+
+        it("creates a vector with buffer", function () {
+          expect(vector).to.be.instanceOf(Vector3);
+          expect(vector.data).to.be.instanceOf(Float32Array);
+        });
+
+        it("initializes buffer correctly", function () {
+          _(arr).each(function (v, i) {
+            expect(vector.data[i]).to.be.closeTo(v, DELTA);
+          });
+        });
+      });
+
+      describe("object based creation", function () {
+        var o, vector;
+        o = {
+          x: Math.E,
+          y: Math.PI,
+          z: Math.SQRT2
+        };
+        vector = new Vector3(o);
+
+        it("creates a vector with buffer", function () {
+          expect(vector).to.be.instanceOf(Vector3);
+          expect(vector.data).to.be.instanceOf(Float32Array);
+        });
+
+        it("initializes buffer correctly", function () {
+          _(["x", "y", "z"]).each(function (v, i) {
+            expect(vector.data[i]).to.be.closeTo(o[v], DELTA);
+          });
+        });
+      });
+
+      describe("value based creation", function () {
+        var arr, vector;
+        arr = [Math.E, Math.PI, Math.SQRT2];
+        vector = new Vector3(arr[0], arr[1], arr[2]);
+
+        it("creates a vector with buffer", function () {
+          expect(vector).to.be.instanceOf(Vector3);
+          expect(vector.data).to.be.instanceOf(Float32Array);
+        });
+
+        it("initializes buffer correctly", function () {
+          _(arr).each(function (v, i) {
+            expect(vector.data[i]).to.be.closeTo(v, DELTA);
+          });
         });
       });
     });
 
-    describe("array based creation", function () {
-      var arr, vector;
-      arr = new Float32Array(3);
-      arr.set([Math.E, Math.PI, Math.SQRT2]);
+    describe("accessors", function () {
+      var o, vector;
+      o = {
+        x: Math.PI,
+        y: Math.E,
+        z: Math.SQRT2
+      };
+      vector = new Vector3(o);
 
-      vector = new Vector3(arr);
-
-      it("creates new buffer view", function () {
-        expect(vector.data).to.be.instanceOf(Float32Array);
-        expect(vector.data).not.to.be.equal(arr);
+      it("getters access appropriate properties", function () {
+        _(o).each(function (v, k) {
+          expect(vector[k]).to.be.closeTo(o[k], DELTA);
+        });
       });
 
-      it("initializes buffer with values from original buffer", function () {
-        _([0, 1, 2]).each(function (i) {
-          expect(vector.data[i]).to.be.equal(arr[i]);
+      it("setters modify appropriate properties", function () {
+        var mod = {
+          x: o.y,
+          y: o.z,
+          z: o.x
+        };
+
+        _(mod).each(function (v, k) {
+          vector[k] = v;
+        });
+
+        _(["x", "y", "z"]).each(function (k, i) {
+          expect(vector.data[i]).to.be.closeTo(mod[k], DELTA);
+        });
+      });
+    });
+
+    describe("interface", function () {
+      var vector = new Vector3(1.0, 1.0, 0.0);
+
+      describe("length - vector length", function () {
+        it("returns expected value", function () {
+          expect(vector.length).to.be.equal(Math.SQRT2);
+        });
+      });
+
+      describe("inverted - inversion", function () {
+        it("returns expected value", function () {
+          var negated = vector.inverted();
+
+          expect(negated.x).to.be.equal(-1);
+          expect(negated.y).to.be.equal(-1);
+          expect(negated.z).to.be.equal(0);
+        });
+      });
+
+      describe("add - vector addition", function () {
+        it("works with objects {x, y, z}", function () {
+          var result = vector.add({x: 1, y: 1, z: 1});
+
+          expect(result.x).to.be.equal(2);
+          expect(result.y).to.be.equal(2);
+          expect(result.z).to.be.equal(1);
+        });
+
+        it("works with vectors", function () {
+          var param = new Vector3(1, 1, 1),
+            result = vector.add(param);
+
+          expect(result.x).to.be.equal(2);
+          expect(result.y).to.be.equal(2);
+          expect(result.z).to.be.equal(1);
+        });
+      });
+
+      describe("mul - scalar multiplication", function () {
+        it("returns expected value", function () {
+          var multiplied = vector.mul(3);
+
+          expect(multiplied.x).to.be.equal(3);
+          expect(multiplied.y).to.be.equal(3);
+          expect(multiplied.z).to.be.equal(0);
+        });
+      });
+
+      describe("unit - unit vector", function () {
+        it("returns expected value", function () {
+          var unit = vector.unit();
+
+          expect(unit.x).to.be.closeTo(Math.SQRT1_2, DELTA);
+          expect(unit.y).to.be.closeTo(Math.SQRT1_2, DELTA);
+          expect(unit.z).to.be.equal(0);
+        });
+      });
+
+      describe("sub - vector substraction", function () {
+        it("works with objects {x,y,z}", function () {
+          var result = vector.sub({x: 1, y: 1, z: 1});
+
+          expect(result.x).to.be.equal(0);
+          expect(result.y).to.be.equal(0);
+          expect(result.z).to.be.equal(-1);
+        });
+
+        it("works with vectors", function () {
+          var right = new Vector3(1, 1, 1),
+            result = vector.sub(right);
+
+          expect(result.x).to.be.equal(0);
+          expect(result.y).to.be.equal(0);
+          expect(result.z).to.be.equal(-1);
+        });
+      });
+
+      describe("div - scalar division", function () {
+        it("returns expected value", function () {
+          var result = vector.div(100);
+
+          expect(result.x).to.be.closeTo(0.01, DELTA);
+          expect(result.y).to.be.closeTo(0.01, DELTA);
+          expect(result.z).to.be.equal(0);
+
         });
       });
     });
